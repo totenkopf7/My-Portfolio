986a0ecb40203299e50f58924682cff99c22eaa2
totenkopf7 <kurdishlearner2018@gmail.com>
2026-06-14 22:39:25 +0300

    Debugging and testing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,10 +12,21 @@ export default function Home({innerRef}) {
    return (
       <Box ref={innerRef} component={'main'} display={'flex'} flexDirection={{xs: 'column', md: 'row'}} alignItems={'center'}
            justifyContent={'center'} minHeight={'calc(100vh - 175px)'} id={'home'}>
-         <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{background: info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '40vh'}}
-              height={{xs: '35vh', md: '40vh'}}
-              borderRadius={'50%'} p={'0.75rem'} mb={{xs: '1rem', sm: 0}} mr={{xs: 0, md: '2rem'}}/>
-         <Box>
+<Box
+  className={classNames(Style.avatar, Style.shadowed)}
+  alt={'image of developer'}
+  style={{ background: info.gradient }}
+  component={'img'}
+  src={me}
+  width={{ xs: '35vh', md: '40vh' }}
+  height={{ xs: '35vh', md: '40vh' }}
+  objectFit="cover"
+  borderRadius={'50%'}
+  p={'0.75rem'}
+  mb={{ xs: '1rem', sm: 0 }}
+  mr={{ xs: 0, md: '2rem' }}
+/>
+<Box>
             <h1>   
                Hi, I'm                                                                                        <span style={{background: info.gradient, WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent'}}>{info.firstName}</span><span className={Style.hand}>🤚</span>
             </h1>
